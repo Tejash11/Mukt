abe5a0f790f955fe5a31b41fe9a5d49db602226e
Tejash <tejashth1103@gmail.com>
2022-08-23 01:14:12 +0530

    refactored notification fragment

--- app/src/main/java/com/example/mukt/Notification.java
+++ app/src/main/java/com/example/mukt/Notification.java
@@ -1,14 +1,82 @@
 package com.example.mukt;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-public class Notification extends AppCompatActivity {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Notification#newInstance} factory method to
+ * create an instance of this fragment.
+ *
+ */
+public class Notification extends Fragment {
+
+    ImageView backbtn;
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Notification.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Notification newInstance(String param1, String param2) {
+        Notification fragment = new Notification();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public Notification() {
+        // Required empty public constructor
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notification);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_notification, container, false);
+
+        backbtn = view.findViewById(R.id.backbutton);
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getContext(),MainActivity.class);
+                startActivity(i);
+
+            }
+        });
+
+        return view;
     }
 }
